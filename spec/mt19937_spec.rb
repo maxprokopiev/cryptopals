12bb5450498f6d418d1ceb51e308104d0f6fa76d
Max Prokopyev <me@maxprokopiev.com>
2021-01-12 23:48:02 +0100

    Add a test for MT19937 output

---
+++ spec/mt19937_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe Cryptopals::MT19937 do
+  describe "#extract_number" do
+    it "generates the same sequence with the same seed value" do
+      mt = described_class.new(1)
+      s1 = []
+      (0..10).each { s1 << mt.extract_number }
+
+      mt = described_class.new(1)
+      s2 = []
+      (0..10).each { s2 << mt.extract_number }
+
+      expect(s1).to eq(s2)
+
+      expect(s1).to eq([1791095845, 4282876139, 3093770124, 4005303368, 491263, 550290313, 1298508491, 4290846341, 630311759, 1013994432, 396591248])
+    end
+  end
+end
